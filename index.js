f208b6e2f5eb2d18f1f004a425347d8846d99d06
Bruno Quadrotti de Freitas <bruno.freitas@fluig.com>
2022-06-02 10:09:34 -0300

    feat: add node proxy

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = 'api.fluig.local';
+const cors = require('cors');
+
+const UNIVERSO_API = 'https://universo.fluig.io';
+
+app.use(cors());
+
+app.use('/process-management/api/v2', createProxyMiddleware({
+  target: UNIVERSO_API,
+  changeOrigin: true
+}));
+
+// Start Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
